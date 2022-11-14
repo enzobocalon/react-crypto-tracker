b98f09310f09f32c742dc7c4ca967404bbd06e17
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-14 20:50:19 -0300

    refactor: changed to match pages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
-export const getData = async () => {
-  const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd');
-  return response;
+export const getDataByPage = async (page: number) => {
+  const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`);
+  return response.data;
+}
+
+export const getDataLength = async () => {
+  const response = await axios.get(`https://api.coingecko.com/api/v3/coins/list`);
+  if (response.data instanceof Array) {
+    return response.data.length;
+  }
 }
