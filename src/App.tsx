6dfa130723bddefb69ea6638de5f069d4f32e8e5
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-13 23:46:05 -0300

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { GlobalStyle } from './global'
+
+function App() {
+  return (
+    <>
+    <h1>teste</h1>
+    <GlobalStyle />
+    </>
+  )
+}
+
+export default App
