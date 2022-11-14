34e28b7f447a47624bd648250d17547c51f6e4f3
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-14 20:50:00 -0300

    feat: Coins interface

---
+++ src/utils/interface.ts
@@ -0,0 +1,9 @@
+export interface ICoins {
+  id: string,
+  name: string,
+  image: string,
+  current_price: number,
+  price_change_percentage_24h: number,
+  price_change_percentage_1h_in_currency: number,
+  price_change_percentage_7d_in_currency: number,
+}
