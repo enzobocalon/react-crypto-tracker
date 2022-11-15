227a27556ded4e39af58720f6fabc01623de9d67
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-15 13:32:42 -0300

    feat: add new prop

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -6,4 +6,5 @@ export interface ICoins {
   price_change_percentage_24h: number,
   price_change_percentage_1h_in_currency: number,
   price_change_percentage_7d_in_currency: number,
+  isFavorite: boolean
 }
