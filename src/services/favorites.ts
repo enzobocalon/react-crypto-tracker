8461d7a6a9ebd896064e883e6c16a8c5774dd33c
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-15 13:33:18 -0300

    feat: add, remove and search favorites on local storage

--- src/services/favorites.ts
+++ src/services/favorites.ts
@@ -1,8 +1,24 @@
-export const saveCoin = () => {
-
-
+export const loadFavorites = () => {
+  const result = localStorage.getItem('coins');
+  if (result) {
+    return JSON.parse(result);
+  }
 }
 
-export const loadCoin = () => {
+export const saveFavorite = (state: string) => {
+  const favorites = loadFavorites();
+  const items = favorites ? favorites : [];
+
+  if (!favorites) { 
+    items.push(state);
+    localStorage.setItem('coins', JSON.stringify(items));
+  }
   
+  const canBeFavorited = favorites.every((coin: string) => coin !== state);
+  if (canBeFavorited) {
+    items.push(state);
+    localStorage.setItem('coins', JSON.stringify(items));
+  } else {
+    localStorage.setItem('coins', JSON.stringify(favorites.filter((coin: string) => coin !== state)));
+  }
 }
