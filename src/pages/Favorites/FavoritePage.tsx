47fbc622232c1ee206afed3e4a8d0208fd3e4d35
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-16 13:35:45 -0300

    feat: pages

---
+++ src/pages/Favorites/FavoritePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Favorites from '../../components/Favorites/Favorites'
+import Menu from '../../components/Menu/Menu'
+
+const FavoritePage = () => {
+  return (
+    <>
+      <Menu />
+      <Favorites />
+    </>
+  )
+}
+
+export default FavoritePage
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Coins from '../../components/Coins/Coins'
+import Menu from '../../components/Menu/Menu'
+
+const Home = () => {
+  return (
+    <>
+      <Menu />
+      <Coins />
+    </>
+  )
+}
+
+export default Home
