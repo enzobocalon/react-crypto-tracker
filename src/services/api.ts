1b990e4e63ffad21b703dbf178f797cb40969016
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-15 22:18:44 -0300

    feat: search coin by id

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,3 +11,8 @@ export const getDataLength = async () => {
     return response.data.length;
   }
 }
+
+export const getDataById = async (id: string) => {
+  const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`)
+  return response.data;
+}
