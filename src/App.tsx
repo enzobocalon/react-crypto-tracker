9342330fd51160d45bc911314397aa011c49e447
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-16 13:36:32 -0300

    feat: added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Coins from './components/Coins/Coins'
 import Menu from './components/Menu/Menu'
+import { Routes, Route} from 'react-router-dom';
 import { GlobalStyle } from './global'
+import Home from './pages/Home/Home';
+import FavoritePage from './pages/Favorites/FavoritePage';
 
 function App() {
   return (
     <>
-    <Menu />
-    <Coins />
-    <GlobalStyle />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<FavoritePage />} />
+        </Routes>
+      <GlobalStyle />
     </>
   )
 }
